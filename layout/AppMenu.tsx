1ccf41d635264e7950c39b006385d425c7be7354
luiz.hcsantos <lhchoinski04@gmail.com>
2025-03-05 18:26:20 -0300

    init project

---
+++ layout/AppMenu.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import AppMenuitem from './AppMenuitem';
+import { LayoutContext } from './context/layoutcontext';
+import { MenuProvider } from './context/menucontext';
+import { AppMenuItem } from '@/types';
+
+const AppMenu = () => {
+    const { layoutConfig } = useContext(LayoutContext);
+
+    const model: AppMenuItem[] = [
+        {
+            label: 'Home',
+            items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+        },
+        {
+            label: 'ADMINISTRATOR',
+            items: [{ label: 'Users', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' }]
+        }
+    ];
+
+    return (
+        <MenuProvider>
+            <ul className="layout-menu">
+                {model.map((item, i) => {
+                    return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> : <li className="menu-separator"></li>;
+                })}
+            </ul>
+        </MenuProvider>
+    );
+};
+
+export default AppMenu;
